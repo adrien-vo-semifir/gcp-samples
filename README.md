f1155287389478c7d4c4d6d22c2a10127bfbd6f4
Adrien Vossough <adrien.vossough@semifir.com>
2023-05-15 16:16:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Samples GCP
+
+## Description
+
+Exemples de code pour utiliser les services GCP.
+
+## Prérequis
+
+- [Python 3.7](https://www.python.org/downloads/release/python-370/)
+
+## Installation
+
+```bash
+poetry install
+```
+
+Placer le fichier avec vos accès GCP dans le dossier `credentials` et le nommer `credentials.json`.
+Le répertoire `credentials` est ignoré par git.
+
+## Exécution
+
+```bash
+poetry run python -m app
+```
---
+++ app/__main__.py
@@ -0,0 +1,28 @@
+from google.cloud import storage
+from google.oauth2 import service_account
+
+
+def upload_blob(bucket_name: str, source_file_name:str, destination_blob_name: str):
+    """Permet d'uploader un fichier dans une bucket GCP
+
+    Args:
+        bucket_name (str): Nom de la bucket GCP où stocker le fichier
+        source_file_name (str): Fichier à uploader
+        destination_blob_name (str): Nom du fichier dans la bucket
+    """
+
+    # Initialiser le client
+    storage_client = storage.Client.from_service_account_json('****.json')
+
+
+    # récupérer la bucket
+    bucket = storage_client.bucket(bucket_name)
+
+    # Créer un blob dans la bucket
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+# Usage
+upload_blob('mon bucket', 'fichiers/sample.txt', 'test/sample.txt')
